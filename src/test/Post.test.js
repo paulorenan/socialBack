d746f78c63adf68c47d2d5691bcc9a2081afa87b
Paulo Renan <able.paulorenan@gmail.com>
2022-04-23 01:04:54 -0300

    test post route

---
+++ src/test/Post.test.js
@@ -0,0 +1,250 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../../server');
+
+const { Post } = require('../models');
+const testMockData = require('./testMockData');
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('Test create post route', () => {
+  describe('With valid token', () => {
+    before(async () => {
+      sinon.stub(Post, 'create').resolves(testMockData.createPost);
+    });
+    after(() => {
+      Post.create.restore();
+    });
+    it('should return the post', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .post('/api/posts')
+        .set('Authorization', testMockData.token)
+        .send(testMockData.post);
+      expect(chaiHttpResponse.status).to.be.equal(201);
+      console.log(chaiHttpResponse.body);
+      expect(chaiHttpResponse.body).to.have.property('content');
+      expect(chaiHttpResponse.body).to.have.property('image');
+    });
+  });
+  describe('With invalid token', () => {
+    it('should return 401', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .post('/api/posts')
+        .set('Authorization', 'invalid token')
+        .send(testMockData.post);
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body).to.have.property('error');
+      expect(chaiHttpResponse.body.error).to.be.equal('Invalid token');
+    });
+  });
+  describe('With no token', () => {
+    it('should return 401', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .post('/api/posts')
+        .send(testMockData.post);
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body).to.have.property('error');
+      expect(chaiHttpResponse.body.error).to.be.equal('No token provided');
+    });
+  });
+});
+
+describe('Test get all posts route', () => {
+  describe('Should return all posts', () => {
+    before(async () => {
+      sinon.stub(Post, 'findAll').resolves(testMockData.allPosts);
+    });
+    after(() => {
+      Post.findAll.restore();
+    });
+    it('should return all posts', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .get('/api/posts');
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.an('array');
+      expect(chaiHttpResponse.body).to.have.lengthOf(2);
+      chaiHttpResponse.body.forEach((post) => {
+        expect(post).to.have.property('id');
+        expect(post).to.have.property('content');
+        expect(post).to.have.property('image');
+        expect(post).to.have.property('userId');
+        expect(post).to.have.property('createdAt');
+        expect(post).to.have.property('updatedAt');
+      });
+    });
+  });
+});
+
+describe('Test update post route', () => {
+  describe('With valid token', () => {
+    before(async () => {
+      sinon.stub(Post, 'findOne').resolves(testMockData.post);
+      sinon.stub(Post, 'update').resolves(testMockData.post);
+    });
+    after(() => {
+      Post.findOne.restore();
+      Post.update.restore();
+    });
+    it('should return the updated post', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .put('/api/posts/1')
+        .set('Authorization', testMockData.token)
+        .send(testMockData.post);
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.have.property('content');
+      expect(chaiHttpResponse.body).to.have.property('image');
+    });
+  });
+  describe('With invalid token', () => {
+    it('should return 401', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .put('/api/posts/1')
+        .set('Authorization', 'invalid token')
+        .send(testMockData.updatePost);
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body).to.have.property('error');
+      expect(chaiHttpResponse.body.error).to.be.equal('Invalid token');
+    });
+  });
+  describe('With no token', () => {
+    it('should return 401', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .put('/api/posts/1')
+        .send(testMockData.post);
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body).to.have.property('error');
+      expect(chaiHttpResponse.body.error).to.be.equal('No token provided');
+    });
+  });
+  describe('With invalid post id', () => {
+    before(async () => {
+      sinon.stub(Post, 'findOne').resolves(null);
+    });
+    after(() => {
+      Post.findOne.restore();
+    });
+    it('should return 404', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .put('/api/posts/1000')
+        .set('Authorization', testMockData.token)
+        .send(testMockData.post);
+      expect(chaiHttpResponse.status).to.be.equal(404);
+      expect(chaiHttpResponse.body).to.have.property('error');
+      expect(chaiHttpResponse.body.error).to.be.equal('Post not found');
+    });
+  });
+  describe('With no authorization', () => {
+    before(async () => {
+      sinon.stub(Post, 'findOne').resolves(testMockData.noAuthPost);
+    });
+    after(() => {
+      Post.findOne.restore();
+    });
+    it('should return 403', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .put('/api/posts/2')
+        .set('Authorization', testMockData.token)
+        .send(testMockData.post);
+      expect(chaiHttpResponse.status).to.be.equal(403);
+      expect(chaiHttpResponse.body).to.have.property('error');
+      expect(chaiHttpResponse.body.error).to.be.equal('You are not allowed to edit this post');
+    });
+  });
+});
+
+describe('Test delete post route', () => {
+  describe('With valid token', () => {
+    before(async () => {
+      sinon.stub(Post, 'findOne').resolves(testMockData.post);
+      sinon.stub(Post, 'destroy').resolves([1]);
+    });
+    after(() => {
+      Post.findOne.restore();
+      Post.destroy.restore();
+    });
+    it('should return the deleted post', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .delete('/api/posts/1')
+        .set('Authorization', testMockData.token);
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.an('array');
+    });
+  });
+  describe('With invalid token', () => {
+    it('should return 401', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .delete('/api/posts/1')
+        .set('Authorization', 'invalid token');
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body).to.have.property('error');
+      expect(chaiHttpResponse.body.error).to.be.equal('Invalid token');
+    });
+  });
+  describe('With no token', () => {
+    it('should return 401', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .delete('/api/posts/1');
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body).to.have.property('error');
+      expect(chaiHttpResponse.body.error).to.be.equal('No token provided');
+    });
+  });
+  describe('With invalid post id', () => {
+    before(async () => {
+      sinon.stub(Post, 'findOne').resolves(null);
+    });
+    after(() => {
+      Post.findOne.restore();
+    });
+    it('should return 404', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .delete('/api/posts/1000')
+        .set('Authorization', testMockData.token);
+      expect(chaiHttpResponse.status).to.be.equal(404);
+      expect(chaiHttpResponse.body).to.have.property('error');
+      expect(chaiHttpResponse.body.error).to.be.equal('Post not found');
+    });
+  });
+  describe('With no authorization', () => {
+    before(async () => {
+      sinon.stub(Post, 'findOne').resolves(testMockData.noAuthPost);
+    });
+    after(() => {
+      Post.findOne.restore();
+    });
+    it('should return 403', async () => {
+      const chaiHttpResponse = await chai.request(app)
+        .delete('/api/posts/2')
+        .set('Authorization', testMockData.token);
+      expect(chaiHttpResponse.status).to.be.equal(403);
+      expect(chaiHttpResponse.body).to.have.property('error');
+      expect(chaiHttpResponse.body.error).to.be.equal('You are not allowed to delete this post');
+    });
+  });
+});
+
+describe('Test get post by user id route', () => {
+  before(async () => {
+    sinon.stub(Post, 'findAll').resolves(testMockData.allPosts);
+  });
+  after(() => {
+    Post.findAll.restore();
+  });
+  it('should return all user posts', async () => {
+    const chaiHttpResponse = await chai.request(app)
+      .get('/api/posts/user/1');
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.an('array');
+    chaiHttpResponse.body.forEach((post) => {
+      expect(post).to.have.property('id');
+      expect(post).to.have.property('content');
+      expect(post).to.have.property('userId');
+      expect(post).to.have.property('createdAt');
+      expect(post).to.have.property('updatedAt');
+      expect(post.userId).to.be.equal(1);
+    });
+  });
+});
--- src/test/testMockData.js
+++ src/test/testMockData.js
@@ -1,72 +1,109 @@
-const user = {
-  id: 1,
-  name: 'John Doe',
-  nickName: 'johndoe',
-  email: 'test@test.com',
-  image: 'test.png',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-};
-
-const userLogin = {
-  email: 'test@test.com',
-  password: '123456',
-};
-
-const wrongUserLogin = {
-  email: 'wrongemail@email.com',
-  password: 'wrongpassword',
-};
-
-const createUser = {
-  name: 'John Doe',
-  nickName: 'johndoe',
-  email: 'test@test.com',
-  password: '123456',
-};
-
-const wrongCreateUser = {
-  name: 'Jo',
-  nickName: 'john doe',
-  email: 'john',
-  password: '12',
-};
-
-const allUsers = [
-  {
+module.exports = {
+  user: {
     id: 1,
     name: 'John Doe',
     nickName: 'johndoe',
     email: 'test@test.com',
-    image: '',
+    image: 'test.png',
     createdAt: new Date(),
     updatedAt: new Date(),
   },
-  {
-    id: 2,
+
+  userLogin: {
+    email: 'test@test.com',
+    password: '123456',
+  },
+
+  wrongUserLogin: {
+    email: 'wrongemail@email.com',
+    password: 'wrongpassword',
+  },
+
+  createUser: {
+    name: 'John Doe',
+    nickName: 'johndoe',
+    email: 'test@test.com',
+    password: '123456',
+  },
+
+  wrongCreateUser: {
+    name: 'Jo',
+    nickName: 'john doe',
+    email: 'john',
+    password: '12',
+  },
+
+  allUsers: [
+    {
+      id: 1,
+      name: 'John Doe',
+      nickName: 'johndoe',
+      email: 'test@test.com',
+      image: '',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      nickName: 'janedoe',
+      email: 'test2@test.com',
+      image: '',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ],
+
+  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsIm5pY2tOYW1lIjoidGVzdCIsImlhdCI6MTY1MDY4MzE1NSwiZXhwIjoxNjUxMjg3OTU1fQ.HbbFNPJDAlur3nYOtDKmkro5WfW20QSG9fmzCo8rob0",
+
+  updateUser: {
     name: 'Jane Doe',
     nickName: 'janedoe',
-    email: 'test2@test.com',
+  },
+
+  createPost: {
+    content: 'Test post',
     image: '',
+  },
+
+  allPosts: [
+    {
+      id: 1,
+      content: 'Test post',
+      image: '',
+      userId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      content: 'Test post 2',
+      image: '',
+      userId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ],
+
+  post: {
+    id: 1,
+    content: 'Test post',
+    image: '',
+    userId: 3,
     createdAt: new Date(),
     updatedAt: new Date(),
   },
-];
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsIm5pY2tOYW1lIjoidGVzdCIsImlhdCI6MTY1MDY4MzE1NSwiZXhwIjoxNjUxMjg3OTU1fQ.HbbFNPJDAlur3nYOtDKmkro5WfW20QSG9fmzCo8rob0"
+  updatePost: {
+    content: 'Test post',
+  },
 
-const updateUser = {
-  name: 'Jane Doe',
-  nickName: 'janedoe',
-};
-
-module.exports = {
-  user,
-  userLogin,
-  wrongUserLogin,
-  createUser,
-  wrongCreateUser,
-  allUsers,
-  token,
-  updateUser,
-};
+  noAuthPost: {
+    id: 2,
+    content: 'Test post 2',
+    image: '',
+    userId: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+}
