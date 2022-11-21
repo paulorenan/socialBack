c2ace07343c8e5da2f1fd318c8ba9009932a9e33
Paulo Renan <able.paulorenan@gmail.com>
2022-11-20 21:10:45 -0300

    test to deploy 2

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express'); //test
+const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const UserRouter = require('./src/routes/UserRoute');
